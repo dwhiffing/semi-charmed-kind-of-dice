93ebd3d4a9c9f10adc02c299311a1fa3eab7f69b
Daniel Whiffing <danielwhiffing@gmail.com>
2025-09-02 15:52:05 -0400

    Merges die/card with parent component

--- src/components/Cards.ts
+++ src/components/Cards.ts
@@ -1,8 +1,6 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: xxx */
 import { createElement } from '../utils/createElement'
-import { state } from '../utils/state'
-import { Card } from './Card'
-// import { state } from '../utils/state'
+import { checkGoal, state } from '../utils/state'
 
 export const Cards = () => {
   const cards = createElement('div', { className: 'cards' })
@@ -17,4 +15,28 @@ export const Cards = () => {
   state.addUpdate('cards', update)
 
   return cards
+}
+
+const Card = (index: number) => {
+  const card = createElement('div', { className: 'card' })
+  const variant = createElement('div')
+  const variantLabel = createElement('div')
+  const valueLabel = createElement('div')
+
+  const update = () => {
+    const _card = state.cards[index]
+    variantLabel.innerHTML = `${_card.goal}:`
+    valueLabel.innerHTML = `${_card.value}`
+    card.classList.toggle('completable', checkGoal(_card))
+  }
+
+  state.addUpdate('cards', update)
+  state.addUpdate('dice', update)
+  update()
+
+  variant.append(variantLabel, valueLabel)
+
+  card.append(variant)
+
+  return card
 }
--- src/components/Dice.ts
+++ src/components/Dice.ts
@@ -1,7 +1,7 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: xxx */
 import { createElement } from '../utils/createElement'
-import { Die } from './Die'
-import { state } from '../utils/state'
+import { colors } from '../constants'
+import { state, toggleDieSelected } from '../utils/state'
 
 export const Dice = () => {
   const dice = createElement('div', { className: 'dice' })
@@ -18,4 +18,56 @@ export const Dice = () => {
   state.addUpdate('dice', update)
 
   return dice
+}
+
+const Die = (index: number) => {
+  const die = createElement('div', {
+    className: 'die',
+    onclick: () => toggleDieSelected(index),
+  }) as HTMLDivElement
+  const number = createElement('div', {
+    className: 'die-number',
+  }) as HTMLDivElement
+  const cat = document
+    .querySelector(`#cat svg`)!
+    .cloneNode(true) as SVGSVGElement
+  cat.classList.add('die-number')
+
+  let dieSvg = createElement('svg')
+  die.append(number, cat, dieSvg)
+  let lastSides = -1
+
+  const update = () => {
+    const _die = state.dice[index]
+    if (!_die) return
+
+    die.style.color = colors[_die.sides]
+    die.style.opacity = _die.roll == null || _die.roll === 1 ? '0.5' : '1'
+    die.classList.remove('d4', 'd6', 'd8', 'd10', 'd12', 'd20')
+    die.classList.add(`d${_die.sides}`)
+
+    die.classList.toggle('selected', _die.selected)
+    die.classList.toggle('die-number-cat', _die.roll === 1)
+    number.textContent = _die.roll ? `${_die.roll}` : ''
+
+    if (lastSides !== _die.sides) {
+      const _dieSvg = document
+        .querySelector(`#d${_die.sides} svg`)!
+        .cloneNode(true) as SVGSVGElement
+      lastSides = _die.sides
+      dieSvg.replaceWith(_dieSvg)
+    }
+
+    if (!_die.selected)
+      die.classList.toggle(
+        'rolling',
+        state.status === 'rolling' && _die.roll == null,
+      )
+  }
+
+  state.addUpdate('dice', update)
+  state.addUpdate('status', update)
+  update()
+
+  return die
 }

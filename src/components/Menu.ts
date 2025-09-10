db84902540481fc438d0669e0999178c95c047e0
Daniel Whiffing <danielwhiffing@gmail.com>
2025-09-09 21:56:52 -0400

    Adds menu credit

--- src/components/Menu.ts
+++ src/components/Menu.ts
@@ -4,6 +4,7 @@ import { startGame, state } from '../state'
 export const Menu = () => {
   const btnStart = createElement('button', 'Start Game') as HTMLButtonElement
   const title = createElement('h2', { className: 'title' }, 'Jynx Dice')
+  const credits = createElement('div', 'By Daniel Whiffing')
   const lastScore = createElement('div', 'Last Score: 0')
   const highScore = createElement('div', 'High Score: 0')
   const buttons = createElement('div', { className: 'buttons' }, btnStart)
@@ -12,6 +13,7 @@ export const Menu = () => {
     'div',
     { className: 'menu' },
     title,
+    credits,
     lastScore,
     highScore,
     buttons,
@@ -24,8 +26,12 @@ export const Menu = () => {
     const visible = state.status !== 'menu' && state.status !== 'lost'
     container.style.opacity = visible ? '0' : '1'
     container.style.pointerEvents = visible ? 'none' : 'auto'
+    credits.style.marginBottom = '10px'
     highScore.textContent = `High Score: ${state.highScore}`
-    lastScore.textContent = state.points ? `Last Score: ${state.points}` : ''
+    lastScore.textContent = `Last Score: ${state.points}`
+
+    highScore.style.display = state.highScore ? 'block' : 'none'
+    lastScore.style.display = state.points ? 'block' : 'none'
   }
 
   btnStart.onclick = () => startGame()
